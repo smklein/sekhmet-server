f64a1839373900b97ede2c4f46729c5dc6d6d757
Sean Klein <seanmarionklein@gmail.com>
2018-05-20 12:48:58 -0700

    Set direction of GPIO pin while exported

--- src/gpio/mod.rs
+++ src/gpio/mod.rs
@@ -17,7 +17,11 @@ impl Gpio {
         };
 
         println!("Gpio::Setting direction of LED pin...");
-        io.led_pin.set_direction(Direction::Out).unwrap();
+        io.led_pin.with_exported(|| {
+            try!(io.led_pin.set_direction(Direction::Out));
+            try!(io.led_pin.set_value(1));
+            Ok(())
+        }).unwrap();
         println!("Gpio::Set direction of LED pin...");
 
         io
