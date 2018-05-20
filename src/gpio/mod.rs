b5362d9f1615f93a4f303b18d46eedc7b531eb77
Sean Klein <seanmarionklein@gmail.com>
2018-05-20 13:19:44 -0700

    Leave GPIO pin exported

--- src/gpio/mod.rs
+++ src/gpio/mod.rs
@@ -16,29 +16,19 @@ impl Gpio {
             led_pin: Pin::new(4),
         };
 
-        println!("Gpio::Setting direction of LED pin...");
-        io.led_pin.with_exported(|| {
-            try!(io.led_pin.set_direction(Direction::Out));
-            try!(io.led_pin.set_value(1));
-            Ok(())
-        }).unwrap();
-        println!("Gpio::Set direction of LED pin...");
+        io.led_pin.export().unwrap();
+        io.led_pin.set_direction(Direction::Out).unwrap();
+        io.led_pin.set_value(1).unwrap();
 
         io
     }
 
     pub fn led_on(&self) {
-        self.led_pin.with_exported(|| {
-            self.led_pin.set_value(1).unwrap();
-            Ok(())
-        }).unwrap();
+        self.led_pin.set_value(1).unwrap();
     }
 
     pub fn led_off(&self) {
-        self.led_pin.with_exported(|| {
-            self.led_pin.set_value(0).unwrap();
-            Ok(())
-        }).unwrap();
+        self.led_pin.set_value(0).unwrap();
     }
 
     pub fn led_toggle(&self, duration: Duration) {
